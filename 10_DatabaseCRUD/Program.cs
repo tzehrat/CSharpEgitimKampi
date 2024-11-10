f2e34c0278adfe1c810363ab3bd79cf095bf7c2c
tzehrat <zehrtaskn@gmail.com>
2024-11-10 18:41:26 +0300

    ..

--- 10_DatabaseCRUD/Program.cs
+++ 10_DatabaseCRUD/Program.cs
@@ -13,7 +13,7 @@ namespace _10_DatabaseCRUD
     {
         static void Main(string[] args)
         {
-            //CRUD operations
+            //CRUD operations Example
 
             Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
             Console.WriteLine();
